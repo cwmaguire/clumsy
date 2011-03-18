34d9f06ed2437f8edc545e28362b869376ca0c00
Chris Maguire <cwmaguire@gmail.com>
2011-03-17 22:20:44 -0700

    removed old test files; should have renamed through git I guess.

--- test/clumsy/prompt-test.clj
+++
@@ -1,8 +0,0 @@
-(ns clumsy.prompt-test
-  (:use clumsy.prompt clojure.test))
-
-; test that strings, functions and keywords work in a prompt
-(deftest prompt-works
-  (is (= "1 ." (prompt {:a 1} [:a " " (fn [p] ".")]))))
-
-(run-tests )
