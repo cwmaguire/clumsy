00248911b943af613bd8124bc3609248e26279bd
Chris Maguire <cwmaguire@gmail.com>
2011-03-17 22:42:13 -0700

    Added clojure.contrib depend.; removed /test/clumsy/core.clj that was created by lein

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clumsy "0.0.1-SNAPSHOT"
   :description "A mud in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"] [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- test/clumsy/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clumsy.test.core
-  (:use [clumsy.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
